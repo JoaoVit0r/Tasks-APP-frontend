9c395f5c4aeb267b8c94b87227aedd072e36c461
JoaoVit0r <joao_cunha_16@outlook.com>
2019-10-31 12:41:22 -0300

    start html

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo App</h1>
+        <div class="add">
+          <input type="text" placeholder="Adicione um novo todo" class="description"></input>
+          <button class="fas fa-plus-square" id="buttonAdd"></button>
+        </div>
       </header>
+      <section class="topics">
+        <ul>
+          <li>
+            <h2>To Do</h2>
+            <ul id="to-do" class="itens"></ul>
+          </li>
+          <li>
+            <h2>Doing</h2>
+            <ul id="doing" class="itens"></ul>
+          </li>
+          <li>
+            <h2>Done</h2>
+            <ul id="done" class="itens"></ul>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
